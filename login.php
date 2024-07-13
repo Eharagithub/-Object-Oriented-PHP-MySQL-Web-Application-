6d7b7426d356fd1caa953d48f921ec05386f57b2
UPANI SINGHAPURA <140133331+Eharagithub@users.noreply.github.com>
2024-07-13 23:44:47 +0530

    login and authentication UI

--- login.php
+++ login.php
@@ -1,31 +1,62 @@
 <?php include('server.php') ?>
 <!DOCTYPE html>
-<html>
+<html lang="en" dir="ltr">
+
 <head>
-  <title>User Login</title>
-  <link rel="stylesheet" type="text/css" href="regstyle.css">
+    <meta charset="UTF-8">
+    <title>Login and Registration Form</title>
+    <link rel="stylesheet" href="login.css">
+    <!-- Fontawesome CDN Link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
+
 <body>
-  <div class="header">
-  	<h2>Login</h2>
-  </div>
-  	 
-  <form method="post" action="login.php">
-  <?php include('errors.php'); ?>
-  	<div class="input-group">
-  		<label>Username</label>
-  		<input type="text" name="username">
-  	</div>
-  	<div class="input-group">
-  		<label>Password</label>
-  		<input type="password" name="password">
-  	</div>
-  	<div class="input-group">
-  		<button type="submit" class="btn" name="login_user">Login</button>
-  	</div>
-  	<p>
-  		Not yet a member? <a href="register.php">Sign up</a>
-  	</p>
-  </form>
+    <div class="container">
+        <input type="checkbox" id="flip">
+        <div class="cover">
+            <div class="front">
+                <img src="images/frontImg.jpg" alt="">
+                <div class="text">
+                    <span class="text-1">Every new friend is a <br> new adventure</span>
+                    <span class="text-2">Let's get connected</span>
+                </div>
+            </div>
+            <div class="back">
+                <div class="text">
+                    <span class="text-1">Complete miles of journey <br> with one step</span>
+                    <span class="text-2">Let's get started</span>
+                </div>
+            </div>
+        </div>
+        <div class="forms">
+            <div class="form-content">
+                <div class="login-form">
+                    <div class="title">Login</div>
+                    <form method="post" action="login.php">
+                        <?php include('errors.php'); ?>
+                        <div class="input-boxes">
+                            <div class="input-box">
+                                <i class="fas fa-user"></i>
+                                <input type="text" name="username" placeholder="Enter your username" required>
+                            </div>
+                            <div class="input-box">
+                                <i class="fas fa-lock"></i>
+                                <input type="password" name="password" placeholder="Enter your password" required>
+                            </div>
+                            <div class="text"><a href="#">Forgot password?</a></div>
+                            <div class="button input-box">
+                                <input type="submit" class="btn" name="login_user" value="Login">
+                            </div>
+                            <div class="text sign-up-text">Don't have an account? <label for="flip"><a href="register.php">Sign up</a></label></div>
+                        </div>
+                    </form>
+                </div>
+
+                
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
--- register.php
+++ register.php
@@ -4,39 +4,64 @@
 
 <head>
     <title>User Registration</title>
-    <link rel="stylesheet" type="text/css" href="regstyle.css">
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="login.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
 <body>
-    <div class="header">
-        <h2>Register</h2>
-    </div>
+    <div class="container">
+        <input type="checkbox" id="flip">
+        <div class="cover">
+            <div class="front">
+                <img src="images/frontImg.jpg" alt="">
+                <div class="text">
+                    <span class="text-1">Every new friend is a <br> new adventure</span>
+                    <span class="text-2">Let's get connected</span>
+                </div>
+            </div>
 
-    <form method="post" action="register.php">
-    <?php include('errors.php'); ?>
-        <div class="input-group">
-            <label>Username</label>
-            <input type="text" name="username" value="<?php echo isset($username) ? $username : ''; ?>">
+            <div class="back">
+                <img class="backImg" src="images/backImg.jpg" alt="">
+                <div class="text">
+                    <span class="text-1">Complete miles of journey <br> with one step</span>
+                    <span class="text-2">Let's get started</span>
+                </div>
+            </div>
         </div>
-        <div class="input-group">
-            <label>Email</label>
-            <input type="email" name="email" value="<?php echo isset($email) ? $email : ''; ?>">
-        </div>
-        <div class="input-group">
-            <label>Password</label>
-            <input type="password" name="password_1">
-        </div>
-        <div class="input-group">
-            <label>Confirm password</label>
-            <input type="password" name="password_2">
-        </div>
-        <div class="input-group">
-            <button type="submit" class="btn" name="reg_user">Register</button>
-        </div>
-        <p>
-            Already a member? <a href="login.php">Sign in</a>
-        </p>
-    </form>
+
+        <div class="forms">
+            <div class="form-content">
+                <div class="signup-form">
+                    <div class="registration-form">
+                        <div class="title">Register</div>
+                        <form method="post" action="register.php">
+                            <?php include('errors.php'); ?>
+                            <div class="input-boxes">
+                                <div class="input-box">
+                                    <i class="fas fa-user"></i>
+                                    <input type="text" name="username" placeholder="Enter your username" value="<?php echo isset($username) ? $username : ''; ?>" required>
+                                </div>
+                                <div class="input-box">
+                                    <i class="fas fa-envelope"></i>
+                                    <input type="email" name="email" placeholder="Enter your email" value="<?php echo isset($email) ? $email : ''; ?>" required>
+                                </div>
+                                <div class="input-box">
+                                    <i class="fas fa-lock"></i>
+                                    <input type="password" name="password_1" placeholder="Enter your password" required>
+                                </div>
+                                <div class="input-box">
+                                    <i class="fas fa-lock"></i>
+                                    <input type="password" name="password_2" placeholder="Confirm your password" required>
+                                </div>
+                                <div class="button input-box">
+                                    <input type="submit" class="btn" name="reg_user" value="Register">
+                                </div>
+                                <div class="text sign-up-text">Already a member? <label for="flip"><a href="login.php">Login</a></label></div>
+                            </div>
+                        </form>
+                    </div>
 </body>
 
 </html>
